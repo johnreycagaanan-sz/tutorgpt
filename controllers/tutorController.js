36e8d2dc596b9ede04de53ef9eda76b221462fd1
unknown <john.cagaanan@supportzebra.com>
2022-12-14 13:06:24 +0800

    Added jwt and bcryptjs packages

--- controllers/tutorController.js
+++ controllers/tutorController.js
@@ -1,5 +1,22 @@
 const Tutor = require('../models/Tutor');
+const crypto = require('crypto');
 const path = require('path');
+
+const sendTokenResponse = (user, statusCode, res) => {
+    const token = tutor.getSignedJwtToken();
+    const options = {
+        expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    }
+
+    if(process.env.NODE_ENV === 'production')  {
+        options.secure = true
+    }
+    res
+        .status(statusCode)
+        .cookie('token', token, options)
+        .json({success: true, token})
+}
 
 const login = async(req, res, next) => {
     const {userName, password } = req.body
--- models/Tutor.js
+++ models/Tutor.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const validator = require('validator')
+const validator = require('validator');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
+
 
 const RatingSchema = new Schema({
     rating: {
@@ -99,6 +103,34 @@ const TutorSchema = new Schema({
     timestamps: true
 })
 
+TutorSchema.pre('save', async function(next) {
+    if(!this.isModified) next();
+
+    const salt = await bcrypt.genSalt(10);
+
+    this.password = await bcrypt.hash(this.password, salt);
+    
+    next();
+})
+
+TutorSchema.methods.getSignedJwtToken = function() {
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+        expiresIn:process.env.JWT_EXPIRE
+    })
+}
+
+TutorSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+TutorSchema.methods.getResetPasswordToken = function() {
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+    this.resetPasswordExpire = Date.now() + 10 * 60 *1000
+
+    return resetToken;
+}
 
 
 
