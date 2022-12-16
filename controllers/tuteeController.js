e610e4b4e51e28f2cd1cc62d93ee1616976dba60
unknown <john.cagaanan@supportzebra.com>
2022-12-16 13:27:02 +0800

    Added tuteeController

--- controllers/tuteeController.js
+++ controllers/tuteeController.js
@@ -1,9 +1,9 @@
-const Tutor = require('../models/Tutor');
+const Tutee = require('../models/Tutee');
 const crypto = require('crypto');
 const path = require('path');
 
-const sendTokenResponse = (tutor, statusCode, res) => {
-    const token = tutor.getSignedJwtToken();
+const sendTokenResponse = (tutee, statusCode, res) => {
+    const token = tutee.getSignedJwtToken();
     const options = {
         expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true
@@ -23,14 +23,14 @@ const login = async(req, res, next) => {
 
     if(!userName || !password) throw new Error ('Please provide username and password')
 
-    const tutor = await Tutor.findOne({userName}).select('+password')
+    const tutee = await Tutee.findOne({userName}).select('+password')
     
-    if(!tutor) throw new error ('Invalid credentials')
+    if(!tutee) throw new error ('Invalid credentials')
 
-    const isMatch = await tutor.matchPassword(password);
+    const isMatch = await tutee.matchPassword(password);
 
     if(!isMatch) throw new Error('Invalid credentails')
-    sendTokenResponse(tutor, 200, res)
+    sendTokenResponse(tutee, 200, res)
 }
 
 const logout = async(req, res, next) => {
@@ -46,23 +46,23 @@ const logout = async(req, res, next) => {
 }
 
 const forgotPassword = async(req, res, next) => {
-    const tutor = await Tutor.findOne({userName: req.body.userName})
+    const tutee = await Tutee.findOne({userName: req.body.userName})
 
-    if(!tutor) throw new Error('No tutor found');
+    if(!tutee) throw new Error('No tutee found');
 
-    const resetToken = tutor.getResetPasswordToken();
+    const resetToken = tutee.getResetPasswordToken();
 
     try {
-        await tutor.save({ validateBeforeSave: false })
+        await tutee.save({ validateBeforeSave: false })
         res
             .status(200)
             .setHeader('Content-Type', 'application/json')
             .json({succes: true, msg: `Password has been reset with token: ${resetToken}`})
     } catch (err) {
-        tutor.resetPasswordToken = undefined;
-        tutor.resetPasswordExpire = undefined;
+        tutee.resetPasswordToken = undefined;
+        tutee.resetPasswordExpire = undefined;
 
-        await tutor.save({ validateBeforeSave: false })
+        await tutee.save({ validateBeforeSave: false })
         throw new Error(`Failed to save new password`)
     }
 }
@@ -70,123 +70,121 @@ const forgotPassword = async(req, res, next) => {
 const resetPassword = async(req, res, next) => {
     const resetPasswordToken = crypto.createHash('sha256').update(req.query.resetToken).digest('hex');
     console.log(resetPasswordToken)
-    const tutor = await Tutor.findOne({
+    const tutee = await Tutee.findOne({
         resetPasswordToken,
         resetPasswordExpire: { $gt: Date.now() }
     })
     
 
-    if(!tutor) throw new Error('Invalid token');
+    if(!tutee) throw new Error('Invalid token');
 
-    tutor.password = req.body.password;
-    tutor.resetPasswordExpire = undefined;
-    tutor.resetPasswordToken = undefined;
+    tutee.password = req.body.password;
+    tutee.resetPasswordExpire = undefined;
+    tutee.resetPasswordToken = undefined;
 
-    await tutor.save();
+    await tutee.save();
 
-    sendTokenResponse(tutor, 200, res)
+    sendTokenResponse(tutee, 200, res)
 }
 
 const updatePassword = async(req, res, next) => {
-    const tutor = await Tutor.findById(req.tutor.id).select('+password');
+    const tutee = await Tutee.findById(req.tutee.id).select('+password');
 
-    const passwordMatches = await tutor.matchPassword(req.body.password);
+    const passwordMatches = await tutee.matchPassword(req.body.password);
 
     if(!passwordMatches) throw new Error('Password is incorrect');
-    tutor.password = req.body.newPassword
+    tutee.password = req.body.newPassword
 
-    await tutor.save();
-    sendTokenResponse(tutor, 200, res)
+    await tutee.save();
+    sendTokenResponse(tutee, 200, res)
 }
 
-const getTutors = async(req, res, next) => {
+const getTutees = async(req, res, next) => {
     const filter = {};
     const options = {};
     if (Object.keys(req.body).length){
         const {
-            subject,
-            schedule,
-            tutorName,
+            tuteeName,
+            email,
             limit,
-            sortByAvailability
+            sortByName
         } = req.query;
         
-        if (subject) filter.subject = true;
-        if (schedule) filter.schedule = true;
-        if (tutorName) filter.tutorName = true;
+        if (tuteeName) filter.tuteeName = true;
+        if (email) filter.email = true;
 
         if (limit) options.limit = limit;
-        if (sortByAvailability) options.sort = {
-            availability: sortByAvailability === 'asc' ? 1: -1
+        if (sortByName) options.sort = {
+            name: sortByName === 'asc' ? 1: -1
         }
 
     }
     try {
-        const tutors = await Tutor.find({}, filter, options);
+        const tutees = await Tutee.find({}, filter, options);
         res
             .status(200)
             .setHeader('Content-Type', 'application/json')
-            .json(tutors)
+            .json(tutees)
     } catch (err) {
-        throw new Error(`Error retrieving tutors: ${err.message}`);
+        throw new Error(`Error retrieving tutees: ${err.message}`);
     }
 }
 
 const postTutor = async(req, res, next) => {
     try {
-        const tutor = await Tutor.create(req.body);
+        const tutee = await Tutee.create(req.body);
         res
             .status(201)
             .setHeader('Content-Type', 'application/json')
-            .json(tutor)
+            .json(tutee)
     } catch (err) {
-        throw new Error(`Error creating tutor: ${err.message}`);
+        throw new Error(`Error creating tutee: ${err.message}`);
     }
     
 }
 
 const deleteTutors = async (req, res, next) => {
     try {
-        await Tutor.deleteMany();
+        await Tutee.deleteMany();
         res
             .status(200)
             .setHeader('Content-Type', 'application/json')
             .json({success:true, msg: 'Tutors removed'})
     } catch (err) {
-        throw new Error(`Error removing tutors: ${err.message}`);
+        throw new Error(`Error removing tutees: ${err.message}`);
     }
    
 }
 
 const getTutor = async(req, res, next) => {
     try {
-        const tutor = await Tutor.findById(req.params.tutorId);
+        const tutee = await Tutee.findById(req.params.tuteeId);
         res
             .status(200)
             .setHeader('Content-Type', 'application/json')
-            .json(tutor)
+            .json(tutee)
     } catch (err) {
-        throw new Error(`Error retrieving tutor ${req.params.tutorId}: ${err.message}`);
+        throw new Error(`Error retrieving tutee ${req.params.tuteeId}: ${err.message}`);
     }
     
 }
 
 const deleteTutor = async(req, res, next) => {
     try {
-        await Tutor.findByIdAndDelete(req.params.tutorId);
+        await Tutee.findByIdAndDelete(req.params.tuteeId);
         res
             .status(200)
             .setHeader('Content-Type', 'application/json')
-            .json({success:true, msg: `Deleting tutor: ${req.params.tutorId}`})
+            .json({success:true, msg: `Deleting tutee: ${req.params.tuteeId}`})
     } catch (err) {
-        throw new Error(`Error deleting tutor ${req.params.tutorId}: ${err.message}`);
+        throw new Error(`Error deleting tutee ${req.params.tuteeId}: ${err.message}`);
     }
     
 }
 
 const updateTutor = async(req, res, next) => {
     try {
-        const tutor = await Tutor.findByIdAndUpdate(req.params.tutorId,{
+        const tutee = await Tutee.findByIdAndUpdate(req.params.tuteeId,{
             $set: req.body
         },{
             new: true
@@ -194,9 +192,9 @@ const updateTutor = async(req, res, next) => {
         res
             .status(200)
             .setHeader('Content-Type', 'application/json')
-            .json(tutor)
+            .json(tutee)
     } catch (err) {
-        throw new Error(`Error updating tutor ${req.params.tutorId}: ${err.message}`)
+        throw new Error(`Error updating tutee ${req.params.tuteeId}: ${err.message}`)
     }
     
 };
@@ -218,7 +216,7 @@ const updateTutor = async(req, res, next) => {
 //         file.mv(filePath, async (err) => {
 //         if(err) throw new Error(`Problem uploading photo: ${err.message}`);
 
-//         await Tutor.findByIdAndUpdate(req.params.tutorId, {image: file.name})
+//         await Tutee.findByIdAndUpdate(req.params.tuteeId, {image: file.name})
 //         res
 //         .status(200)
 //         .setHeader('Content-Type', 'application/json')
@@ -228,7 +226,7 @@ const updateTutor = async(req, res, next) => {
 
 
 module.exports = {
-    getTutors,
+    getTutees,
     postTutor,
     deleteTutors,
     login,
