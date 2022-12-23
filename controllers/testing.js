94eb456cf21bbbd18f3cbff47404956ca9bfb7e3
John Rey <john.cagaanan@supportzebra.com>
2022-12-23 08:03:16 +0800

    Tutor GPT final

--- controllers/testing.js
+++ controllers/testing.js
@@ -1,16 +1,15 @@
 const twilio = require('twilio')
 const dotenv = require('dotenv');
 dotenv.config({ path: '../config/config.env'});
-const schedule = require('node-schedule')
 const { zonedTimeToUtc, utcToZonedTime, format } = require('date-fns-tz')
+const schedule = require('node-schedule')
 
 
 const sendTo2 = '+639061783380'
-const sendTime = new Date('2022-12-21T16:40:00+0800')
+const sendTime = new Date('2022-12-23T08:45:00+0800')
 const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 console.log(sendTime)
 const zonedDate = utcToZonedTime(sendTime, 'Asia/Kuala_Lumpur')
-console.log(`zonedDate is ${zonedDate} and should after 5 mins`)
 sendTime.setMinutes(zonedDate.getMinutes() - 60)
 console.log(`new SendTime is: ${sendTime}`)
 console.log(process.env.TWILIO_FROM)
@@ -27,12 +26,6 @@ client.messages
           .done();
 
 const j = schedule.scheduleJob(sendTime, function() {
-    console.log(`reached J`)
-    console.log(process.env.TWILIO_FROM)
-    console.log(process.env.TWILIO_ACCOUNT_SID)
-    console.log(process.env.TWILIO_AUTH_TOKEN)
-    console.log(`new SendTime is: ${sendTime}`)
-
     client.messages
           .create({
               body: 'Hello, this is a scheduled message!',
--- middlewares/authTutor.js
+++ middlewares/authTutor.js
@@ -6,11 +6,10 @@ const protectedRouteForTutor = async(req, res, next) => {
         token = req.headers.authorization.split(' ')[1];
     }
     if(!token) throw new Error('Not authorized to access this route')
+    
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         req.tutor = await Tutor.findById(decoded.id)
-        // console.log(decoded.id)
-        // console.log(`req.tutor in authTutor is : ${req.tutor}`)
         next()
     } catch (err) {
         throw new Error(err.mesage)
